1e3409794e37ac2a59414d6ce8191c2d7b49638f
Berkay Zaim <88087972+berkayzaimdev@users.noreply.github.com>
2023-01-09 14:11:21 +0300

    Update Config.java

--- PatikaKlonu/Helper/Config.java
+++ PatikaKlonu/Helper/Config.java
@@ -5,5 +5,5 @@ public class Config
     public static final String PROJECT_TITLE = "Patika.dev";
     public static final String DB_URL = "jdbc:mysql://localhost/patika";
     public static final String DB_USERNAME = "root";
-    public static final String DB_PASSWORD = "_QTifv27J1FPJ16z";
+    public static final String DB_PASSWORD = "";
 }
